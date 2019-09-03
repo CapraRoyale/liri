19b507d32634217a5817b4c8656cd5797738ac7d
CapraRoyale <siege.lehman@gmail.com>
2019-09-02 18:30:31 -0700

    Added Moment and Chalk, and formatted JSON responses

--- liri.js
+++ liri.js
@@ -1,73 +1,158 @@
+//Include .env configuration file
 require("dotenv").config();
-var keys = require("./keys.js");
+//create resource variables
+const keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
-var Spotify = new Spotify(keys.spotify);
-var axios = require("axios");
-//
+const spotify = new Spotify(keys.spotify);
+const axios = require("axios");
+const fs = require("fs");
+const chalk = require("chalk");
+const moment = require("moment");
+const space = " ";
+// Create variable to contain text enter in command line
 var newCommand = process.argv[2];
-//
-var spotifyThisSong = function(song) {
-        queryurl = ("https://api.spotify.com/v1/tracks/" + song);
-        return queryurl;
+// Create functions to contain queries
+var spotifyThis = function(searchType, song, thisLimit) {
+        spotify.search({
+                type: searchType,
+                query: song,
+                limit: thisLimit
+            },
+            function(err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                //
+                console.log(
+                    chalk.bgWhite.black(data.tracks.items[0].name) +
+                    space + "by" + space +
+                    chalk.bgYellow.black(data.tracks.items[0].artists[0].name)
+                );
+                console.log(
+                    chalk.red(
+                        data.tracks.items[0].album.name
+                    )
+                );
+                console.log(
+                    chalk.blue(data.tracks.items[0].preview_url)
+                );
+            })
     }
     //
 var moviethis = function(movie) {
-        queryurl = ("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy");
-        return queryurl;
-    }
     //
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
+        .then(function(response) {
+            // handle success
+            // console.log("SUCCESS:");
+            console.log(
+                chalk.bgBlue(response.data.Title) + space +
+                chalk.bgRed(response.data.Ratings[0].Value + space + "on IMDB") + space +
+                chalk.bgGreen(response.data.Ratings[1].Value + space + "on Rotten Tomatoes")
+            );
+            console.log(chalk.blue(response.data.Country) + space +
+                chalk.italic(response.data.Language) + space
+            );
+            console.log(chalk.bgWhite.black.italic(response.data.Plot) + space);
+            console.log(
+                response.data.Actors
+            );
+        })
+        .catch(function(error) {
+            // handle error
+            console.log("ERROR: ");
+            console.log(error);
+        })
+        .finally(function() {
+            // always executed
+            // console.log("Complete");
+        });
+};
+//
 var bandsInTown = function(artist) {
-    queryurl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-    return queryurl;
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function(response) {
+            // handle success
+            // console.log("SUCCESS:");
+            for (let i = 0; i < response.data.length; i++) {
+
+                console.log(chalk.bgWhite.black(response.data[i].venue.name));
+                console.log(response.data[i].venue.city + "," + space + response.data[i].venue.country)
+                console.log(response.data[i].datetime);
+                console.log("");
+            }
+        })
+        .catch(function(error) {
+            // handle error
+            console.log("ERROR: ");
+            console.log(error);
+        })
+        .finally(function() {
+            // always executed
+            // console.log("Complete");
+        });
 };
 //
 var querySource = function(source) {
     axios.get(source)
         .then(function(response) {
             // handle success
+            // console.log("SUCCESS:");
             console.log(response);
         })
         .catch(function(error) {
             // handle error
+            console.log("ERROR: ");
             console.log(error);
         })
         .finally(function() {
             // always executed
+            console.log("Complete");
         });
 }
-
-
 
 switch (newCommand) {
     //
     case "concert-this":
         //
-        var artist = process.argv[3];
-        //
         console.log("BANDS");
         //
-        querySource(bandsInTown(artist));
+        var artist = process.argv[3];
+        //
+        bandsInTown(artist);
         //
         break;
+        //
     case "spotify-this-song":
         //
-        console.log(newCommand);
+        console.log("SONG");
         //
         var song = process.argv[3];
         //
-        querySource(spotifyThisSong(song));
+        spotifyThis("track", song, 1);
+        //
+        // console.log(song);
         //
         break;
+        //
     case "movie-this":
-        //      OMDB API
+        //OMDB API
         console.log("MOVIES");
         //
         var movie = process.argv[3];
         //
-        querySource(moviethis(movie))
-            //
+        moviethis(movie);
+        //
         break;
+        //
     case "do-what-it-says":
+        //
         console.log("SOMETHING");
+        //
+        fs.readFile('random.txt', 'utf8', function(err, contents) {
+            console.log(contents);
+        });
+        //
         break;
+        //
 };
